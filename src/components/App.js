44cd4bcaf369c98d78be5999959ac5e0b00b39bf
befabry <fabrybenjamin5@gmail.com>
2020-02-20 20:58:30 +0100

    add ResourceList
    functional component to fetch datas

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ResourceList from "./ResourceList";
 
 const App = () => {
     const [resource, setResource] = useState("posts");
@@ -14,7 +15,7 @@ const App = () => {
                 <button onClick={() => setResource("posts")}>Posts</button>
                 <button onClick={() => setResource("todos")}>Todos</button>
             </div>
-            {resource}
+            <ResourceList resource={resource} />
         </div>
     );
 };
---
+++ src/components/ResourceList.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const ResourceList = () => {
+    const [resources, setResources] = useState([]);
+
+    const fetchResource = async () => {
+        const response = await axios.get(
+            `https://jsonplaceholder.typicode.com/${this.props.resource}`
+        );
+
+        this.setState({ resource: response.data });
+    };
+
+    return <div>{resources.length}</div>;
+};
+
+export default ResourceList;
