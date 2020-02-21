4c79d42c222a91f8cb4b9ce42450490033a1dcea
befabry <fabrybenjamin5@gmail.com>
2020-02-21 22:29:34 +0100

    Update ResourceList.js
    add useEffect

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,18 +1,31 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const ResourceList = () => {
+const ResourceList = ({ resource }) => {
     const [resources, setResources] = useState([]);
 
-    const fetchResource = async () => {
+    const fetchResource = async (resource) => {
         const response = await axios.get(
-            `https://jsonplaceholder.typicode.com/${this.props.resource}`
+            `https://jsonplaceholder.typicode.com/${resource}`
         );
 
-        this.setState({ resource: response.data });
+        setResources(response.data);
     };
 
-    return <div>{resources.length}</div>;
+    //Everytime it is rendered we recreate the array (deps)
+    //If the array (deps) changes, it calls the callback function
+    //To call only once => empty array
+    useEffect(() => {
+        fetchResource(resource);
+    }, [resource]);
+
+    return (
+        <ul>
+            {resources.map((record) => (
+                <li key={record.id}>{record.title}</li>
+            ))}
+        </ul>
+    );
 };
 
 export default ResourceList;
